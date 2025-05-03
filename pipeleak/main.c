a507cab7477299329f728b41816ada9d823835fe
Sebastián Chaves Durán <sebastian.chavesduran@ucr.ac.cr>
2025-05-03 02:31:58 -0600

    estructura y orden basico de carpetas

---
+++ pipeleak/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ROWS 5
+#define COLS 4
+
+int pipe[ROWS][COLS] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9, 10, 11, 12},
+    {13, 14, 15, 16},
+    {17, 18, 19, 20}
+};
